2bc61a9386e024a1ee5d4a7fd0fc871379c0bbf7
mostar <mominahmad93@gmail.com>
2018-07-30 12:49:45 -0500

    set variables

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var magicword = document.getElementById("magicword");
+var guessesLeftEL = document.getElementById("guessesLeft")
+var charactersused = document.getElementById("charactersUsed")
+var wins = document.getElementById("wins");
+var loses = document.getElementById("loses");
+
+loses.textContent = 'test'.split("");
